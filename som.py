06b07b5c7196a2ef1c1b5f11829d6ccb10c61d8f
Lucas Pereira <lucaass_mc@hotmail.com>
2021-06-01 18:54:45 -0300

    Traduçao

--- som.py
+++ som.py
@@ -7,42 +7,42 @@ import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 # %matplotlib inline
 
-# read the image
+# leia a imagem
 img = plt.imread('./img/fishing.jpg')
 
-# reshaping the pixels matrix
+# remodelando a matriz de pixels
 pixels = np.reshape(img, (img.shape[0]*img.shape[1], 3))
 
-# SOM initialization and training
-print('training...')
+# Inicialização e treinamento do SOM
+print('Treinamento...')
 som = MiniSom(3, 3, 3, sigma=1., learning_rate=0.2, neighborhood_function='bubble')  # 3x3 = 9 final colors
 som.random_weights_init(pixels)
-starting_weights = som.get_weights().copy()  # saving the starting weights
+starting_weights = som.get_weights().copy()   # salvando os pesos iniciais
 som.train_random(pixels, 500)
 
-print('quantization...')
-qnt = som.quantization(pixels)  # quantize each pixels of the image
-print('building new image...')
+print('Quantização...')
+qnt = som.quantization(pixels)  # quantize cada pixel da imagem
+print('Construindo nova imagem...')
 clustered = np.zeros(img.shape)
-for i, q in enumerate(qnt):  # place the quantized values into a new image
+for i, q in enumerate(qnt):  # coloque os valores quantizados em uma nova imagem
     clustered[np.unravel_index(i, dims=(img.shape[0], img.shape[1]))] = q
-print('done.')
+print('Feito.')
 
-# show the result
+# Exibindo resultado
 plt.figure(figsize=(7, 7))
 plt.figure(1)
 plt.subplot(221)
-plt.title('original')
+plt.title('Original')
 plt.imshow(img)
 plt.subplot(222)
-plt.title('result')
+plt.title('Resultado')
 plt.imshow(clustered)
 
 plt.subplot(223)
-plt.title('initial colors')
+plt.title('Cores iniciais')
 plt.imshow(starting_weights, interpolation='none')
 plt.subplot(224)
-plt.title('learned colors')
+plt.title('Cores aprendidas')
 plt.imshow(som.get_weights(), interpolation='none')
 
 plt.tight_layout()
